a09c2d7b02332239cb04258f4f912a26d757415a
anamol04 <sh.anmol23@ncmt.edu.np>
2026-01-27 16:34:56 +0545

    Things done

--- verticleOneDMotionSimulation.js
+++ verticleOneDMotionSimulation.js
@@ -5,7 +5,7 @@ function Simulation(u, a, dt, t_max){
     const result =[];
 
     while(time<=t_max){
-        position+=velocity*dt;
+        position=position+(velocity*dt);
         velocity+=a*dt;
 
         result.push({
@@ -19,4 +19,42 @@ function Simulation(u, a, dt, t_max){
     return result;
 }
 
-console.log(Simulation(0, 9.81, 0.1, 5));
+console.log(`Simulation 1---->`);
+console.log(Simulation(0, 9.81, 1, 5));
+console.log(`<----Simulation 1`);
+
+// console.log(`Simulation 1---->${JSON.stringify(Simulation(0, 9.81, 1, 5))}`);
+
+function Simulation2(u, a, dt, t_max){
+    let velocity=u;
+    let time=0;
+    let position=0;
+    let result=[];
+
+    while(time<=t_max){
+        velocity+=a*dt;
+        position+=velocity*dt;
+        result.push({
+            time: Number(time.toFixed(2)),
+            position: Number(position.toFixed(2)),
+            velocity: Number(velocity.toFixed(2)),
+            acceleration: a
+        })
+        time+=dt;
+    }
+    return result;
+}
+console.log(`Simulation 2---->`);
+console.log(Simulation2(0, 9.81, 0.1, 5));
+console.log(`<----Simulation 2`);
+
+
+console.log(`____________Task Momentum_________________`)
+
+function Momentum(mass, initialSpeed, stopTime){
+    let finalSpeed= 0;
+    let momentum= mass*(finalSpeed-initialSpeed);
+    let force=momentum/stopTime;
+    return(force<5000 ? `You can Safely Stop` : `Don't stop, it's Dangerous!!!`)
+}
+console.log(`Landing Information: ${Momentum(8000, 40, 10)}`)
